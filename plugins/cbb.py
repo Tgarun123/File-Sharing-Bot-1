46b1c833c30ff61bbaa781378e2bbbcb61c2956f
Rolexmawa <138117429+Rolexmawa@users.noreply.github.com>
2023-06-29 18:22:33 +0530

    Update cbb.py

--- plugins/cbb.py
+++ plugins/cbb.py
@@ -10,7 +10,7 @@ async def cb_handler(client: Bot, query: CallbackQuery):
     data = query.data
     if data == "about":
         await query.message.edit_text(
-            text = f"<b>○ Creator : <a href='tg://user?id={5397893493}'>This Person</a>\n○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Source Code : <a href='https://github.com/CodeXBotz/File-Sharing-Bot'>Click here</a>\n○ Channel : @ajbackupp Support Group : @ajbackupp</b>",
+            text = f"<b>○ Language : <code>Python3</code>\n○ Library : <a href='https://docs.pyrogram.org/'>Pyrogram asyncio {__version__}</a>\n○ Channel : @TeluguBadi_Ofcl\n○ Support Group : @TeluguBadi_Ofcl</b>",
             disable_web_page_preview = True,
             reply_markup = InlineKeyboardMarkup(
                 [
